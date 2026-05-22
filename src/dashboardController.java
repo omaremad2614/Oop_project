0475134ed9c3abcb3f69668af939c01bb0c79875
omar <omarex234@gmail.com>
2026-05-22 16:14:59 +0300

    omar

---
+++ src/dashboardController.java
@@ -0,0 +1,9 @@
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+
+public class dashboardController {
+}
